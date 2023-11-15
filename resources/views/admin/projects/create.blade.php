41d2f64486472cc328f73faa9142bea9e1334972
PietroCipolla95 <pietro.cipolla.95@gmail.com>
2023-11-15 17:53:07 +0100

    add type in create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -26,21 +26,40 @@
                 <input type="text" class="form-control" name="title" id="title">
                 <small>Type title</small>
             </div>
+
             <div class="mb-3">
                 <label for="description" class="form-label fw-bold">Description</label>
                 <textarea class="form-control" name="description" id="description" rows="4"></textarea>
                 <small>type description (technology used, purpose etc.)</small>
             </div>
+
+            <div class="mb-3">
+                <label for="type_id" class="form-label fw-bold">Project Type</label>
+                <select name="type_id" id="type_id" class="form-select">
+                    <option selected disabled>Choose category</option>
+
+                    @foreach ($types as $type)
+                        <option value="{{ $type->id }}" {{ $type->id == old('type_id') ? 'selected' : '' }}>
+                            {{ $type->name }}
+                        </option>
+                    @endforeach
+
+
+                </select>
+            </div>
+
             <div class="mb-3">
                 <label for="git_link" class="form-label fw-bold">GitHub</label>
                 <input type="text" class="form-control" name="git_link" id="git_link">
                 <small>GitHub repo link</small>
             </div>
+
             <div class="mb-3">
                 <label for="project_link" class="form-label fw-bold">Project Link</label>
                 <input type="text" class="form-control" name="project_link" id="project_link">
                 <small>project link if any</small>
             </div>
+
             <div class="mb-3">
                 <label for="cover_image" class="form-label fw-bold">Choose Image</label>
                 <input type="file" class="form-control" name="cover_image" id="cover_image" placeholder=""
